148cd1edc784e3bae3f8fd1945da9a4ce5f42dae
musab ibrahim <mosubibrahim96@gmail.com>
2024-09-24 21:11:11 +0200

    signup signin functionality implemented

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -1,80 +1,162 @@
-// controllers/patientController.js
 const patientService = require("../services/patientService");
 const validator = require("validator");
+const path = require("path");
 const response = require("../middleware/response");
 const JWTUtil = require("../middleware/jwt");
-const {upload, uploadSingleImage} = require('../middleware/imageUploader')
+const { uploadSingleImage } = require("../util/cloudinary");
+const { encryptData, compareData } = require("../util/hashData");
+
+// Allowed image types
+const allowedImageTypes = ['.jpg', '.jpeg', '.png'];
+const MAX_IMAGE_SIZE = 2 * 1024 * 1024; // 5 MB
+
+// Helper function to validate image type
+const isValidImageType = (filename) => {
+    const ext = path.extname(filename).toLowerCase();
+    return allowedImageTypes.includes(ext);
+};
+
 class PatientController {
+    /**
+     * @api {post} /patient/signup Register Patient
+     * @apiName RegisterPatient
+     * @apiGroup Patient
+     * @apiDescription Register a new patient
+     * @apiParam {String} name Patient name
+     * @apiParam {String} email Patient email
+     * @apiParam {String} password Patient password
+     * @apiParam {String} gender Patient gender
+     * @apiParam {File} image Patient image
+     * @apiSuccess {String} status Status of the response
+     * @apiSuccess {String} message Message describing the response
+     * @apiSuccess {Object} data Patient data
+     * @apiSuccess {String} data._id Patient id
+     * @apiSuccess {String} data.name Patient name
+     * @apiSuccess {String} data.email Patient email
+     * @apiSuccess {String} data.gender Patient gender
+     * @apiSuccess {String} data.photo Patient photo
+     * @apiSuccess {String} data.imgPId Patient image public id
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "status": "success",
+     *     "message": "Patient registered successfully",
+     *     "data": {
+     *         "_id": "5f7a7a7a7a7a7a7a7a",
+     *         "name": "John Doe",
+     *         "email": "john@example.com",
+     *         "gender": "male",
+     *         "photo": "https://example.com/default-image.jpg",
+     *         "imgPId": "default-image.jpg"
+     *     }
+     * }
+     * @apiErrorExample {json} Error-Response:
+     * {
+     *     "status": "fail",
+     *     "message": "You already have an account with this please login!"
+     * }
+     */
     async register(req, res) {
         try {
-            let { name, email, password, gender } = req.body;
-            let photo;
-            if (!name || !email || !password || !gender) {
-                return response(res,400,'fail',`please fill all the required fields: ${!name ? "name," : ""} ${!email ? "email," : ""} ${!password ? "password," : ""} ${!gender ? "gender" : ""}`);
-            }
-            if (!validator.isAlpha(name, "en-US", { ignore: " " })) {
-                return response(res,400,'fail','Name must contain only letters');
-            }
-            if (name.length < 3 || name.length > 30) {
-                return response(res,400,'fail','Name must be between 3 and 30 characters long');
-            }
-            if (!validator.isEmail(email)) {
-                return response(res,400,'fail','Invalid email address');
-            }
-            if (password.length < 8) {
-                return response(res,400,'fail','Password must be at least 8 characters long');
-            }
-            if (!['male', 'female'].includes(gender.trim().toLowerCase())) {
-                return response(res,400,'fail','Gender must be "male" or "female"');
-            }
-            gender = gender.trim().toLowerCase();
-            let patient = await patientService.registerPatient({ name, email, password, gender });
-            if (patient === 11001) {
-                return response(res,400,'fail','Email already in use');
-            }
-            if(req.file){
-                upload.single("image");
-                req.body.id = patient._id;
-                const results = await uploadSingleImage(req,res);
-                patient.photo = results.secure_url;
-                patient.imageId = results.public_id;
+                let { name, email, password, gender } = req.body;
+                let photo;
+                if (!name || !email || !password || !gender) {
+                    return response(res,400,'fail',`please fill all the required fields: ${!name ? "name," : ""} ${!email ? "email," : ""} ${!password ? "password," : ""} ${!gender ? "gender" : ""}`);
+                }
+                if (!validator.isAlpha(name, "en-US", { ignore: " " })) {
+                    return response(res,400,'fail','Name must contain only letters');
+                }
+                if (name.length < 3 || name.length > 30) {
+                    return response(res,400,'fail','Name must be between 3 and 30 characters long');
+                }
+                if (!validator.isEmail(email)) {
+                    return response(res,400,'fail','Invalid email address');
+                }
+                if (password.length < 8) {
+                    return response(res,400,'fail','Password must be at least 8 characters long');
+                }
+                if (!['male', 'female'].includes(gender.trim().toLowerCase())) {
+                    return response(res,400,'fail','Gender must be "male" or "female"');
+                }
 
-                patient = await patient.save();
-                if (!patient) {
-                    return response(res,500,'fail','Unable to upload you image try again later');   
-                }   
-                req.body.id= undefined;
+                gender = gender.trim().toLowerCase();
+
+                password = await encryptData(password);
+                
+                let patient = await patientService.registerPatient({ name, email, password, gender });
+                if (patient === 11001) {
+                    return response(res,400,'fail','You already have an account with this please login!');
+                }
+                if (req.file) {
+                    req.body.id = patient._id;
+                    const imageBuffer = req.file.buffer;
+                    const filename = req.file.originalname;
+
+                    // Validate image type and size 
+                    if (!isValidImageType(filename)) {
+                        return res.status(400).json({ success: false, message: `Invalid image type. Allowed types are: ${allowedImageTypes.join(", ")}` });
+                    }
+                    if (imageBuffer.length > MAX_IMAGE_SIZE) {
+                        return res.status(400).json({ success: false, message: "Image exceeds maximum size of 5 MB." });
+                    }
+                    const results = await uploadSingleImage(imageBuffer, req.body.id);
+                    
+                    if(!results) {
+                        return response(res,500,'fail','failed to upload image please try again');
+                    }
+                    photo = results.secure_url;
+                    patient.photo = photo;
+                    patient.imgPId = results.public_id;
+                    await patient.save();
+                    req.body.id = undefined;
+                }
+                patient.password = undefined;
+                patient.__v = undefined;
+                const token = JWTUtil.generateToken(patient);
+                res.cookie("token", token, {
+                    httpOnly: true, // Ensure it's accessible only via HTTP (not client-side JS)
+                    secure: process.env.NODE_ENV === "production", // Set secure only in production
+                    maxAge: 3600000 * 24 * 30 // 30 days expiration
+                })
+                response(res, 201, 'success', 'Patient registered successfully', patient);
+            } catch (error) {
+                console.log('====================================');
+                console.log(error);
+                console.log('====================================');
+                response(res,500,'fail','something went wrong');
+        }
+    }
+    async login(req, res) {
+        let { email, password } = req.body;
+        if(!email || !password) {
+            return response(res,400,'fail',`please fill all the required fields: ${!email ? "email! " : ""} ${!password ? "password!" : ""}`);
+        }
+        if(!validator.isEmail(email)) {
+            return response(res,400,'fail','Invalid email address');
+        }
+        if(password.length < 8) {
+            return response(res,400,'fail','Password must be at least 8 characters long');
+        }
+        try {
+            const patient = await patientService.loginPatient(email);
+            if(patient === 404) {
+                return response(res,404,'fail','You do not have an account please register');
+            }
+
+            const isMatch = await compareData(password, patient.password);
+            if(!isMatch) {
+                return response(res,400,'fail','Invalid credentials');
             }
             patient.password = undefined;
             patient.__v = undefined;
             const token = JWTUtil.generateToken(patient);
-            res.cookie("token", token, {
-                httpOnly: true, // Ensure it's accessible only via HTTP (not client-side JS)
-                secure: process.env.NODE_ENV === "production", // Set secure only in production
-                maxAge: 3600000 * 24 * 30 // 30 days expiration
-            })
-            response(res, 201, 'success', 'Patient registered successfully', patient);
-        } catch (error) {
-            console.log('====================================');
-            console.log(error);
-            console.log('====================================');
-            response(res,500,'fail','something went wrong');
-        }
-    }
-
-    async login(req, res) {
-        const { email, password } = req.body;
-        try {
-            const { patient, token } = await patientService.loginPatient(email, password);
-            
             // Set token as a cookie
             res.cookie("token", token, {
                 httpOnly: true, // Ensure it's accessible only via HTTP (not client-side JS)
-                secure: process.env.NODE_ENV === "production", // Set secure only in production
+                secure: process.env.NODE_ENV === "production", // Set secure only in production 
                 maxAge: 3600000 // 1 hour expiration
             });
+            response(res, 200, 'success', 'Patient logged in successfully', patient);
             
-            res.status(200).json({ message: "Login successful", patient });
         } catch (error) {
             res.status(400).json({ error: error.message });
         }
