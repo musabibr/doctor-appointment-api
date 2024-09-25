8a7964c386138c0ad862e539500860cf2945da5f
musab ibrahim <mosubibrahim96@gmail.com>
2024-09-25 15:45:16 +0200

    hot fix

--- src/app.js
+++ src/app.js
@@ -58,11 +58,13 @@ redisClient.on('error', (err) => {
 });
 
 app.all('*', (req, res, next) => {
-    res.status(404 || 401).json({
-        status: 'fail',
-        method: `${req.method}`,
-        message: `Can't find ${req.originalUrl} on this server!`
-    })
+    if (!res.headersSent) {
+        res.status(404 || 401).json({
+            status: 'fail',
+            method: `${req.method}`,
+            message: `Can't find ${req.originalUrl} on this server!`
+        });
+    }
 });
 
 module.exports = app;
