6b368236dccbdcb106929ace7b34621f5644e280
musab ibrahim <mosubibrahim96@gmail.com>
2024-09-25 16:47:24 +0200

    bug fix

--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -2,26 +2,7 @@ const express = require("express");
 const router = express.Router();
 const patientController = require("../controllers/patientController");
 const authMiddleware = require("../middleware/authMiddleware");
-
-const multer = require("multer");
-const multerStorage = multer.memoryStorage();
-const multerFilter = (req, file, cb) => {
-    // Check if the file is an image by checking the mimetype of the file
-    if (file.mimetype.startsWith("image")) {
-        // If the file is an image, call the callback function with the arguments (null, true)
-        cb(null, true);
-    } else {
-        // If the file is not an image, call the callback function with the arguments (new Error('Not an image! Please upload only images.'), false)
-        cb(new Error("Not an image! Please upload only images."), false);
-    }
-};
-
-const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter,
-});
-
-
+const {upload} = require("../middleware/imageUploader");
 // Patient Registration
 router.post("/signup", upload.single("image"),patientController.register);
 // 
